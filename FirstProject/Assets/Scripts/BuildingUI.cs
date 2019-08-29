8fb3e2505e867f260935b58cc11d3328ef2a7bd4
csh <tizkzks96@gmail.com>
2019-08-29 15:42:59 +0900

    UI State 연결

--- FirstProject/Assets/Scripts/BuildingUI.cs
+++ FirstProject/Assets/Scripts/BuildingUI.cs
@@ -4,23 +4,78 @@ using UnityEngine;
 using UnityEngine.UI;
 using GoogleARCore.Examples.HelloAR;
 
+public enum UIState{
+    HOME,
+    ENVIRONMENT,
+    BUILDING
+}
+
 public class BuildingUI : MonoBehaviour
 {
-    public GameObject MainPanel;
+    public GameObject HomePanel;
 
     public GameObject EnvironmentPanel;
 
+    public GameObject BuildingPanel;
+
     public GameObject buildingSlot;
+
+    public UIState UIState { get; set; } = UIState.HOME;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //HomePanel 버튼 연결
+        HomePanel.transform.Find("EnvironmentBtn").GetComponent<Button>().onClick.AddListener(() => { ChangeState(UIState.ENVIRONMENT); });
+        HomePanel.transform.Find("BuildingsBtn").GetComponent<Button>().onClick.AddListener(() => { ChangeState(UIState.BUILDING); });
+
+        //EnvironmentPanel 버튼 연결
+        EnvironmentPanel.transform.Find("Home").GetComponent<Button>().onClick.AddListener(() => { ChangeState(UIState.HOME); });
+
+        //BuildingPanel 버튼 연결
+        BuildingPanel.transform.Find("Home").GetComponent<Button>().onClick.AddListener(() => { ChangeState(UIState.HOME); });
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    /// <summary>
+    /// UI Panel 상태를 체인지한다.
+    /// </summary>
+    public void ChangeState(UIState uiState)
+    {
+        switch (uiState)
+        {
+            case UIState.HOME:
+                HomePanel.SetActive(true);
+
+                EnvironmentPanel.SetActive(false);
+
+                BuildingPanel.SetActive(false);
+                break;
+            case UIState.ENVIRONMENT:
+                HomePanel.SetActive(false);
+
+                EnvironmentPanel.SetActive(true);
+
+                BuildingPanel.SetActive(false);
+                break;
+            case UIState.BUILDING:
+                HomePanel.SetActive(false);
+
+                EnvironmentPanel.SetActive(false);
+
+                BuildingPanel.SetActive(true);
+                break;
+            default:
+                break;
+        }
     }
 
     /// <summary>
